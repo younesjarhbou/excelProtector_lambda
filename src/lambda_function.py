15d22e59659627ab7c49343f5a149792b4113ec9
Admin <admin@192.168.1.27>
2024-12-25 00:42:13 +0100

    add CI CD

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -74,5 +74,5 @@ def lambda_handler(event, context):
     return
     {
         'statusCode': 200,
-        'body': json.dumps('hello from github action')
+        'body': json.dumps('hello from github action + permission')
     }
